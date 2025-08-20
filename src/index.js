5b7bab0ca6892a00ee0512543e3dda9a2abc2f1a
humblezxc <stopenchuk.yaroslav@gmail.com>
2025-08-20 02:30:56 +0200

    Created theme change logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import "./styles/main.scss";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const body = document.body;
+    const savedTheme = localStorage.getItem("theme");
+    const toggleBtn = document.getElementById("theme-toggle");
+
+    if (savedTheme) {
+        body.classList.add(savedTheme);
+    } else {
+        const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        body.classList.add(prefersDark ? "dark-theme" : "light-theme");
+    }
+
+    toggleBtn.addEventListener("click", () => {
+        console.log('here')
+        if (body.classList.contains("light-theme")) {
+            body.classList.replace("light-theme", "dark-theme");
+            localStorage.setItem("theme", "dark-theme");
+        } else {
+            body.classList.replace("dark-theme", "light-theme");
+            localStorage.setItem("theme", "light-theme");
+        }
+    });
+});
